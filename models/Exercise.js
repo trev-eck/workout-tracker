d795f71bf6df0dc3e69ec4744f3de7eed21ac100
Trevor Eckberg <trevor.eckberg@gmail.com>
2021-05-12 23:10:33 -0700

    basic file structure created, models created

---
+++ models/Exercise.js
@@ -0,0 +1,33 @@
+const { Decimal128 } = require('bson');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    
+    type: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    distance: {
+        type: Decimal128,
+    },
+    duration: {
+        type: Number,
+    },
+    weight: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    }
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
